802fb92b498e25e21ecd7af7e819ac3da0263910
Shiran Maduwantha <88098649+shiran2019@users.noreply.github.com>
2024-07-24 12:51:51 +0530

    fetch product details and images in home page

--- src/app/apiManager.js
+++ src/app/apiManager.js
@@ -1,45 +1,43 @@
+
 export const post = async ({ path, requestBody, header = {} }) => {
-    const baseUrl = import.meta.env.VITE_BASE_URL;
-    console.log(baseUrl);
-  
-    const response = await fetch(`${baseUrl}${path}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        ...header,
-      },
-      body: JSON.stringify(requestBody),
-    });
-    const body = await response.json();
-    return body;
-  };
-  
-  
-  export const postFile = async ({ path, file }) => {
-    const baseUrl = import.meta.env.VITE_BASE_URL;
-    console.log(baseUrl);
-    const response = await fetch(`${baseUrl}${path}`, {
-      method: "POST",
-      body: file,
-    });
-    console.log(response);
-    const body = await response.json();
-    return body;
-  };
-  
-  
-  export const get = async ({ path, header = {} }) => {
-    const baseUrl = import.meta.env.VITE_BASE_URL;
-  
-    const response = await fetch(`${baseUrl}${path}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        ...header,
-      },
-    });
-  
-    const body = await response.json();
-    return body;
-  };
-  
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  console.log(baseUrl);
+
+  const response = await fetch(`${baseUrl}${path}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      ...header,
+    },
+    body: JSON.stringify(requestBody),
+  });
+  const body = await response.json();
+  return body;
+};
+
+export const postFile = async ({ path, file }) => {
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  console.log(baseUrl);
+  const response = await fetch(`${baseUrl}${path}`, {
+    method: "POST",
+    body: file,
+  });
+  console.log(response);
+  const body = await response.json();
+  return body;
+};
+
+export const get = async ({ path, header = {} }) => {
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+
+  const response = await fetch(`${baseUrl}${path}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      ...header,
+    },
+  });
+
+  const body = await response.json();
+  return body;
+};
